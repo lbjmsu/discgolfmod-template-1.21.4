8eff7c95c9ed55687bdb620e2cf2f51740a896b9
lbjmsu <107739580+lbjmsu@users.noreply.github.com>
2024-12-23 15:27:15 -0500

    Initial commit

---
+++ src/main/java/ejcraft2/discgolfmod/DiscGolfModClient.java
@@ -0,0 +1,11 @@
+package ejcraft2.discgolfmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DiscGolfModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/ejcraft2/discgolfmod/DiscGolfModDataGenerator.java
@@ -0,0 +1,11 @@
+package ejcraft2.discgolfmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DiscGolfModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
